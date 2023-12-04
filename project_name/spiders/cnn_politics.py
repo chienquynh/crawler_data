d5f9fd6dda5ecac3ecf950485b97d4ac09f47890
chien111 <124616791+chienquynh@users.noreply.github.com>
2023-12-04 08:55:35 +0700

    crawler

--- project_name/spiders/cnn_politics.py
+++ project_name/spiders/cnn_politics.py
@@ -2,19 +2,20 @@ import scrapy
 from scrapy.loader import ItemLoader
 from project_name.items import MyItem
 
-class GuardianSpider(scrapy.Spider):
-    name = 'theguardian'
-    allowed_domains = ['metatext.io/datasets']
-    start_urls = ['https://metatext.io/datasets-list/english-language']
+class MSNBCSpider(scrapy.Spider):
+    name = 'msnbc'
+    allowed_domains = ['msnbc.com']
+    start_urls = ['http://www.msnbc.com/']
+
     visited_urls = set()
 
     def parse(self, response):
         # Extract information from the current page
         loader = ItemLoader(item=MyItem(), response=response)
-        loader.add_css('title', 'title::text')
-        loader.add_css('author', '.dcr-byline span[itemprop="name"]::text')
-        loader.add_css('timestamp', 'time[itemprop="datePublished"]::attr(datetime)')
-        loader.add_css('content', '.js-article__body p::text')
+        loader.add_css('title', 'h2.headline::text')
+        loader.add_css('author', 'span[data_test="author_name"]::text')
+        loader.add_css('timestamp', 'time[data_time_published]::attr(data_time_published)')
+        loader.add_css('content', 'div[itemprop="articleBody"] p::text')
         item = loader.load_item()
         yield item
 
@@ -22,23 +23,16 @@ class GuardianSpider(scrapy.Spider):
         self.visited_urls.add(response.url)
 
         # Follow links to individual articles
-        for link in response.css('.fc-item__title a::attr(href)').getall():
+        for link in response.css('h2.headline a::attr(href)').getall():
             full_link = response.urljoin(link)
             if full_link not in self.visited_urls:
                 yield scrapy.Request(full_link, callback=self.parse_article)
 
-        # Follow pagination links to the next page
-        next_page = response.css('.pagination__item--next a::attr(href)').get()
-        if next_page:
-            next_page_url = response.urljoin(next_page)
-            if next_page_url not in self.visited_urls:
-                yield scrapy.Request(next_page_url, callback=self.parse)
-
     def parse_article(self, response):
-        yield from self._parse_content(response, '.content__headline', '.byline a', '.content__article-body p::text', self.parse_sub_article)
+        yield from self._parse_content(response, 'h1.headline', 'span[data_test="author_name"]', 'div[itemprop="articleBody"] p::text', self.parse_sub_article)
 
     def parse_sub_article(self, response):
-        yield from self._parse_content(response, '.content__headline', '.byline a', '.content__article-body p::text', self.parse_sub_article)
+        yield from self._parse_content(response, 'h1.headline', 'span[data_test="author_name"]', 'div[itemprop="articleBody"] p::text', self.parse_sub_article)
 
     def _parse_content(self, response, title_selector, author_selector, content_selector, callback):
         try:
@@ -46,16 +40,15 @@ class GuardianSpider(scrapy.Spider):
             loader = ItemLoader(item=MyItem(), response=response)
             loader.add_css('title', title_selector)
             loader.add_css('author', author_selector)
-            loader.add_css('timestamp', 'time[itemprop="datePublished"]::attr(datetime)')
+            loader.add_css('timestamp', 'time[data_time_published]::attr(data_time_published)')
             loader.add_css('content', content_selector)
             item = loader.load_item()
             yield item
 
             # Follow links to sub-articles within the current article
-            for sub_link in response.css('.content__subheading a::attr(href)').getall():
+            for sub_link in response.css('a.sub_headline::attr(href)').getall():
                 full_sub_link = response.urljoin(sub_link)
                 if full_sub_link not in self.visited_urls:
                     yield scrapy.Request(full_sub_link, callback=callback)
-
         except Exception as e:
             self.log(f"Error in parsing: {str(e)}")
